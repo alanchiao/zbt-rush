4dad6616b745be7c869eb2a307b5f98ad137c0c4
Alan Chiao <alanchiao@mit.edu>
2014-06-14 11:27:38 -0700

    install meteor google library

--- google_meteor/client/views/map/mapMain.js
+++ google_meteor/client/views/map/mapMain.js
@@ -3,3 +3,19 @@ if (navigator.geolocation){
 		console.log(position);
 	});
 }
+
+if (Meteor.isClient) {
+    GoogleMaps.init( 
+        {
+            'sensor': false
+        },
+        function(){
+            var mapOptions = {
+                zoom: 13,
+                mapTypeId: google.maps.MapTypeId.SATELLITE
+            };
+            map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+            map.setCenter(new google.maps.LatLng(35.363556, 138.730438));
+        }
+    );
+}
