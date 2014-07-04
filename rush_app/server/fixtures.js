69c6ba64af4248e5e7db99f613be34bcd21852ba
Jeffrey Sun <jrsun@mit.edu>
2014-07-03 17:35:20 -0700

    better form formatting and fixtures

--- rush_app/server/fixtures.js
+++ rush_app/server/fixtures.js
@@ -1,35 +1,48 @@
 if (Rides.find().count() === 0){
     Rides.insert({
-        'name':'clueless',
-        'passengers':'3',
-        'phone':'4',
-        'status':'unassigned'
+        name:'Kevin Tian',
+        time: '3:00 PM',
+        passengers: '4',
+        phone: '(510)-378-2423',
+        pickup: 'Next House',
+        dropoff: 'ZBT',
+        status: 'unassigned',
+        comments: 'Mr. Fantastic'
     });
 
     
     Rides.insert({
-        'name':'clueless',
-        'passengers':'3',
-        'phone':'4',
-        'status':'unassigned'
+        name: 'Alan Chiao',
+        time: '3:45 PM',
+        passengers: '1',
+        phone: '(617)-332-4345',
+        pickup: 'Baker',
+        dropoff: 'ZBT',
+        status: 'unassigned',
+        comments: 'Silver Surfer'
     });
 
 
     Rides.insert({
-        'name':'clueless',
-        'passengers':'3',
-        'phone':'4',
-        'status':'unassigned'
+        name: 'Alex Jaffe',
+        passengers: '3',
+        phone: '(535)-343-2312',
+        pickup: 'Maseeh',
+        dropoff: 'ZBT',
+        status: 'unassigned',
+        comments: 'J-j-j-jaffe'
     });
 };
 
 
 if (Drivers.find().count() == 0){
     Drivers.insert({
-        'name':'rick',
-        'passengers':'0',
-        'capacity':'25',
-        'rides':[]
+        name: 'Charles',
+        passengers: '0',
+        phone: '(617)-393-4234',
+        capacity: '5',
+        comments: 'ZBestpresidenT',
+        rides: []
     });
 }
     
