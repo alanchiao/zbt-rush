f04aad366c1afcf2a1537a65d2270e9ae11bbd3a
Jeffrey Sun <jrsun@ubuntu.(none)>
2014-07-04 05:37:39 -0400

    badly coded drawers

--- rush_app/client/views/rick/drivers/driver-form.js
+++ rush_app/client/views/rick/drivers/driver-form.js
@@ -1,4 +1,16 @@
 Template.driverForm.events({
+    'click .driver-drawer-handle': function(e){
+        var target = $('.driver-drawer-target');
+        if(target.is(':hidden')){
+            target.slideDown();
+            $('.driver-drawer-handle').removeClass('glyphicon-chevron-down');
+            $('.driver-drawer-handle').addClass('glyphicon-chevron-up');
+        } else {
+            target.slideUp();
+            $('.driver-drawer-handle').removeClass('glyphicon-chevron-up');
+            $('.driver-drawer-handle').addClass('glyphicon-chevron-down');
+        }
+    },
     'submit form': function(e){
         e.preventDefault();
         var driver = utils.formToJson(e.target);
