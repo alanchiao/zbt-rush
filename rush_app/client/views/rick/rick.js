84bd8b96c315517894027d9a10b6fec6ca274c33
Jeffrey Sun <jrsun@ubuntu.(none)>
2014-07-06 15:35:23 -0400

    multiple ride selection and driver selection

--- rush_app/client/views/rick/rick.js
+++ rush_app/client/views/rick/rick.js
@@ -1,25 +1,59 @@
 //Connects rides and drivers portion of the rick app
+// TODO: change name -> data-id or something similar
 Template.rick.events({
-    'click .ride': function(e){
-        console.log(e.currentTarget);
-        currentRideId = $(e.currentTarget).find('[name=rideId]')[0].innerText;
+    'click [data-js=ride]': function(e){
+        var rideEl = e.currentTarget;
+        if (selectedRides.indexOf(rideEl) == -1){
+            selectRide(rideEl);
+        }
      },
 
-     'click .driver': function(e){
-        console.log(e.currentTarget);
-         var selectedDriverId = $(e.currentTarget).find('[name=driverId]')[0].innerText;
-         if (currentRideId != null){
-
-            var driver = Drivers.find(selectedDriverId).fetch()[0];
-            console.log(driver);
-            Meteor.call('assignRide', currentRideId, selectedDriverId, function(error, id){
-                if (error){
-                    return alert(error.reason);
-                }
-            });
-            currentRideId = null;
-         }
-     }
+    'click [data-js=driver]': function(e){
+        var driverEl = e.currentTarget;
+        if (selectedRides.length == 0 && selectedDrivers.indexOf(driverEl) == -1){
+            selectDriver(driverEl);
+        } else {
+            var driverId = driverEl.dataset['id'];
+            var driver = Drivers.find(driverId).fetch()[0];
+            for (var index in selectedRides){
+                var ride = selectedRides[index];
+                Meteor.call('assignRide', ride.dataset['id'], driverId, function(error, id){
+                    if (error){
+                        return alert(error.reason);
+                    }
+                });
+            }
+            selectedRides = [];
+        }
+    }
 });
 
-var currentRideId;
+var selectRide = function(rideEl){
+    selectedRides.push(rideEl);
+    $(rideEl).addClass('selected');
+}
+
+var deselectRide = function(rideEl){
+    selectedRides.pop(rideEl);
+    $(rideEl).removeClass('selected');
+}
+
+var deselectAllRides = function(){
+    for (var index in selectedRides){
+        var rideEl = selectedRides[index];
+        deselectRide(rideEl);
+    }
+}
+
+var selectDriver = function(driverEl){
+    selectedDrivers.push(driverEl);
+    $(driverEl).addClass('selected');
+}
+
+var deselectDriver = function(driverEl){
+    selectedDrivers.pop(driverEl);
+    $(driverEl).removeClass('selected');
+}
+
+var selectedRides = [];
+var selectedDrivers = [];
