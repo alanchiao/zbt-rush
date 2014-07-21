123586ec4ad341b6b85b74a29872161cba89443b
Alan Chiao <alanchiao@mit.edu>
2014-07-20 17:24:12 -0700

    rename of ride-item. Fix bugs relating to editing everything, not being able to exit edit mode, and being able to select while in edit mode. clean html a little of ride-item

--- rush_app/client/views/rick/rides/ride-item.js
+++ rush_app/client/views/rick/rides/ride-item.js
@@ -1,12 +1,15 @@
 Template.rideItem.events({
   'click [data-js=ride]': function(e){
-    Rides.update(this._id, {$set:{selected: !this.selected}});
+    if(this.editing === false){
+        Rides.update(this._id, {$set:{selected: !this.selected}});
+    }
   },
   'click [data-js=delete]': function(e){
     Rides.remove(this._id);
   },
   'click [data-js="edit"]': function(e){
     e.stopPropagation();
+    console.log(this.editing);
     Rides.update(this._id, {$set:{editing: !this.editing}});
   },
   'submit form': function(e){
@@ -15,3 +18,21 @@ Template.rideItem.events({
     Rides.update(this._id, {$set:options});
   }
 });
+
+Template.rideItem.helpers({
+    editClass: function(){
+        if(this.editing === true){
+            return 'editing'
+        } else {
+            return '';
+        }
+    },
+    selectedClass: function(){
+        if(this.selected === true){
+            return 'selected'
+        } else {
+            return '';
+        }
+    }
+});
+
