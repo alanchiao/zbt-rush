81090551f0b8dd4df09e4cba915748ba46a74b32
Vineel Adusumilli <vineel@adusumilli.org>
2014-08-25 23:53:27 -0700

    Map now updates every 5s.

--- rush_app/client/views/map-ui/map.js
+++ rush_app/client/views/map-ui/map.js
@@ -4,10 +4,22 @@ Template.map.rendered = function(){
     L.mapbox.accessToken = 'pk.eyJ1IjoidmluZWVsIiwiYSI6IlFNdVFXOVkifQ.hIr__TFm_SKAU-5mSruX4g';
     var mapbox = L.mapbox.map('map', 'vineel.i06b0oh9').setView([42.35, -71.11], 15);
 
-    ActiveDrivers.find().forEach(function(driver){
-      var marker = L.circleMarker([parseFloat(driver.lastLatitude), parseFloat(driver.lastLongitude)], {color: 'white', fill: true, fillOpacity: 0.7});
-      marker.addTo(mapbox);
-    });
+    var carLayer = L.layerGroup([]).addTo(mapbox);
+
+    function updateDrivers(){
+      ActiveDrivers.find().forEach(function(driver){
+        carLayer.clearLayers();
+
+        var lat = parseFloat(driver.lastLatitude);
+        var lon = parseFloat(driver.lastLongitude);
+        if (!isNaN(lat) && !isNaN(lon)) {
+          var marker = L.circleMarker([lat, lon, {color: 'white', fill: true, fillOpacity: 1.0}]);
+          carLayer.addLayer(marker);
+        }
+      });
+    }
+
+    setInterval(updateDrivers, 5000);
   });
 };
 
