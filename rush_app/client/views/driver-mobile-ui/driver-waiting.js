c80d1006ae9b2f98e101d4d96b2947e4f2a0f8a4
Jeffrey Sun <jrsun@mit.edu>
2014-08-26 17:58:46 -0400

    driver-mobile-ui name fix

--- rush_app/client/views/driver-mobile-ui/driver-waiting.js
+++ rush_app/client/views/driver-mobile-ui/driver-waiting.js
@@ -1,6 +1,6 @@
 Template.driverWaiting.helpers({
   whenDone: function(){
-  	var defaultMessage = "Thanks for driving, " + this.name + "! Nothing to do yet.";
+  	var defaultMessage = "Thanks for driving, " + Drivers.findOne(this.driverId).name + "! Nothing to do yet.";
     return this.instruction ? this.instruction : defaultMessage;
   },
 });
